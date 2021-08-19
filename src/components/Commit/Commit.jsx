8e96b4ce544347cb74de33480eff783bd4b06233
Gabriel Falcones <gafalcon@espol.edu.ec>
2021-08-19 00:10:06 -0500

    Add commit owner img

--- src/components/Commit/Commit.jsx
+++ src/components/Commit/Commit.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import dayjs from "dayjs";
+import Image from "react-bootstrap/Image";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
@@ -10,7 +11,15 @@ function Commit({ commit }) {
         <a href={commit.url}>{commit.message}</a>
       </h4>
       <p>
-        <a href={commit.author.html_url}>{commit.author.name}</a>
+        <a href={commit.author.html_url}>
+          <Image
+            src={commit.author.avatar_url}
+            roundedCircle
+            width="25"
+            height="25"
+          />
+          <span className="ms-2">{commit.author.name}</span>
+        </a>
         <span> commited {dayjs().to(dayjs(commit.author.date))}</span>
       </p>
     </div>
