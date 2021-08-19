47c4d009d6d69df50c8f82689243cace3c990528
Gabriel Falcones <gafalcon@espol.edu.ec>
2021-08-18 23:09:01 -0500

    Set Repo Title

--- src/components/RepoTitle/RepoTitle.jsx
+++ src/components/RepoTitle/RepoTitle.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-function RepoTitle() {
+function RepoTitle({ repo }) {
   return (
     <div>
-      <h1>RepoTitle</h1>
+      <h1>
+        <a href={`https://github.com/${repo.owner}`}> {repo.owner}</a>/
+        <a href={`https://github.com/${repo.owner}/${repo.name}`}>
+          {repo.name}
+        </a>
+      </h1>
     </div>
   );
 }
