d32b30a99560ec1cb620dc0ff4014d3c9b48efa1
Gabriel Falcones <gafalcon@espol.edu.ec>
2021-08-18 23:09:22 -0500

    Set repolist display

--- src/components/RepoList/RepoList.jsx
+++ src/components/RepoList/RepoList.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-function RepoList() {
+function RepoList({ repos, onRepoSelect }) {
   return (
     <div>
-      <h1>RepoList</h1>
+      <h3>Repositories</h3>
+      <ul>
+        {repos.map((repo) => (
+          <li key={repo.name}>{repo.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
