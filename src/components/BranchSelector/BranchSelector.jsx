6c14a260478accf260207d596bcd7f1a0d25c483
Gabriel Falcones <gafalcon@espol.edu.ec>
2021-08-19 00:43:52 -0500

    Fix branch selecting bug

--- src/components/BranchSelector/BranchSelector.jsx
+++ src/components/BranchSelector/BranchSelector.jsx
@@ -3,15 +3,18 @@ import React from "react";
 import { FloatingLabel, Form } from "react-bootstrap";
 
 function BranchSelector({ branches, currentBranch, onBranchSelect }) {
+  const branchChange = (e) => {
+    onBranchSelect(branches[parseInt(e.target.value)]);
+  };
   return (
     <FloatingLabel controlId="floatingSelect" label="Branch">
       <Form.Select
         aria-label="Floating label select example"
-        onChange={(e) => onBranchSelect(e.target.value)}
+        onChange={branchChange}
         value={currentBranch || ""}
       >
-        {branches.map((branch) => (
-          <option key={branch.commit.sha} value={branch}>
+        {branches.map((branch, i) => (
+          <option key={branch.commit.sha} value={i}>
             {branch.name}
           </option>
         ))}
